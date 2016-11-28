3b4eeb64e4d2d9c6b0715a653521f2c0aaaa0b10
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2016-11-28 15:54:38 +0300

    init

---
+++ Cimbiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cimbiz.MvcApplication" Language="C#" %>
---
+++ Cimbiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cimbiz.Startup))]
+namespace Cimbiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
